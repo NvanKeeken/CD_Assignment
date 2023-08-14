bc01375228711c504718531d75636c92d742cd39
N van Keeken <n.v.k20@gmail.com>
2023-08-14 15:02:51 +0200

    initial commit

---
+++ test_main.py
@@ -0,0 +1,10 @@
+
+from main import index, requirements
+
+def test_index():
+    assert index() == "Welcome, to my final winc assignment: CD!"
+
+def test_requirments():
+    assert requirements() == "The requirments are: build a continuous deployment using Git Actions"
+
+
