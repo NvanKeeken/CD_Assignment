efef8dcc881813df0a6311d5c0df685e70103fe8
N van Keeken <n.v.k20@gmail.com>
2023-08-15 21:09:50 +0200

    updated main

--- test_main.py
+++ test_main.py
@@ -2,7 +2,7 @@
 from main import index, requirements
 
 def test_index():
-    assert index() == "Welcome, to my final winc assignment: CD!It Workt!"
+    assert index() == "Welcome, to my final winc assignment: CD!"
 
 def test_requirments():
     assert requirements() == "The requirments are: build a continuous deployment using Git Actions"
