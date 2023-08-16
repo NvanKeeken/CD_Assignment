4aeaa4b41c99a8d692a22905efd51fb71a6e3773
N van Keeken <n.v.k20@gmail.com>
2023-08-16 20:55:26 +0200

    test change

--- pull.sh
+++
@@ -1,4 +0,0 @@
-cd /home/CD_Assignment
-git pull origin main
-systemctl restart CD_Assignment
-systemctl status CD_Assignment
--- test_main.py
+++ test_main.py
@@ -2,7 +2,7 @@
 from main import index, requirements
 
 def test_index():
-    assert index() == "Welcome, to my final winc assignment: CD! I hope it works!"
+    assert index() == "Welcome, to my final winc assignment: CD! I hope it will work!"
 
 def test_requirments():
     assert requirements() == "The requirments are: build a continuous deployment using Git Actions"
