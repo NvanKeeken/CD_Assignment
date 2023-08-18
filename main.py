f6730d56d03c2412578982dc238ba921df503fc3
NvanKeeken <110396102+NvanKeeken@users.noreply.github.com>
2023-08-18 14:27:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 # Assign a function to be called when the path `/` is requested
 @app.route('/')
 def index():
-    return "Welcome, to my final winc assignment: CD! I hope it will work, yes!"
+    return "Welcome, to my final winc assignment: CD! I hope it will work!"
 
 @app.route('/requirements')
 def requirements():
