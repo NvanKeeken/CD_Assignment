607049b1888713e5137ea5d0f5472c78967bf5d9
N van Keeken <n.v.k20@gmail.com>
2023-08-24 20:04:28 +0200

    added html to flask app

--- main.py
+++ main.py
@@ -19,18 +19,5 @@ def index():
 def about():
     return render_template("about.html", title="About")
 
-# # Import what we need from flask
-# from flask import Flask
 
-# # Create a Flask app inside `app`
-# app = Flask(__name__)
-
-# # Assign a function to be called when the path `/` is requested
-# @app.route('/')
-# def index():
-#     return "Welcome, to my final winc assignment: CD! I hope it will work, yes!"
-
-# @app.route('/requirements')
-# def requirements():
-#     return 'The requirments are: build a continuous deployment using Git Actions'
 
--- test_main.py
+++ test_main.py
@@ -25,14 +25,5 @@ def test_about(client):
     response = client.get("/about")
     assert response.status_code == 200
     assert b"<title>About</title>" in response.data
-    
-# from main import index, requirements
-
-# def test_index():
-#     assert index() == "Welcome, to my final winc assignment: CD! I hope it will work, yes!"
-
-# def test_requirments():
-#     assert requirements() == "The requirments are: build a continuous deployment using Git Actions"
-
 
 
