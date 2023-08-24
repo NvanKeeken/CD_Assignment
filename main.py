ab4260385473af7fb411a07b80b9d0560c09f909
N van Keeken <n.v.k20@gmail.com>
2023-08-24 19:53:21 +0200

    added html to flask app

--- main.py
+++ main.py
@@ -1,15 +1,36 @@
-# Import what we need from flask
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for
 
-# Create a Flask app inside `app`
+__winc_id__ = "9263bbfddbeb4a0397de231a1e33240a"
+__human_name__ = "templates"
+
 app = Flask(__name__)
 
-# Assign a function to be called when the path `/` is requested
-@app.route('/')
-def index():
-    return "Welcome, to my final winc assignment: CD! I hope it will work, yes!"
 
-@app.route('/requirements')
-def requirements():
-    return 'The requirments are: build a continuous deployment using Git Actions'
+@app.route("/home", methods=["GET"])
+def home():
+    return redirect(url_for("index"), 302)
+
+@app.route("/", methods=["GET"])
+def index():
+    return render_template("index.html", title="Index")
+
+
+@app.route("/about", methods=["GET"])
+def about():
+    return render_template("about.html", title="About")
+
+# # Import what we need from flask
+# from flask import Flask
+
+# # Create a Flask app inside `app`
+# app = Flask(__name__)
+
+# # Assign a function to be called when the path `/` is requested
+# @app.route('/')
+# def index():
+#     return "Welcome, to my final winc assignment: CD! I hope it will work, yes!"
+
+# @app.route('/requirements')
+# def requirements():
+#     return 'The requirments are: build a continuous deployment using Git Actions'
 
